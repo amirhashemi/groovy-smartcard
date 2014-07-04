51656ccf501578fabfe0ffe513ff15a26cceee09
Amir Hashemi <amir.hashemik@gmail.com>
2014-07-04 12:54:58 +0200

    Comments Added.

--- Demo1/sc_io.groovy
+++ Demo1/sc_io.groovy
@@ -1,16 +1,21 @@
+//Importing java smartcardio api.
 import javax.smartcardio.*
 
+//Accessing the terminal manager and listing the available terminals on PC.
 def factory = TerminalFactory.getDefault()
 def terminals = factory.terminals().list()
 println "Available Terminals : " + terminals
 
+//Connecting to one of the terminals in the list by its index.
 def terminal = terminals.get(1)
 def card = terminal.connect("T=0")
 println "Connection Status : " + card
 
+//Requesting the card Answer to Reset
 def atr = card.getATR()
 println "**ATR : " + atr.getBytes().encodeHex()
 
+//Sending a select default Card Manager to a javacard insereted.
 def select = "00A4040008A000000003000000"
 def cmd = new CommandAPDU(select.decodeHex())
 def channel = card.getBasicChannel()
@@ -18,4 +23,5 @@ def resp = channel.transmit(cmd)
 println "card << " + select
 println "card >> " + resp.getBytes().encodeHex()
 
+//Disconnecting the communication.
 card.disconnect(false)
